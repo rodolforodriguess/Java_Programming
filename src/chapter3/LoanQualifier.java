496a5cc30c25d243bf0d91ed5781e8a2cd1d88a6
Rodolfo Rodrigues <rodolfo@sk8.com.br>
2019-06-15 16:28:43 -0300

    smooth change

--- src/chapter3/LoanQualifier.java
+++ src/chapter3/LoanQualifier.java
@@ -3,7 +3,7 @@ package chapter3;
 /*
 NESTED IFS:
 * To qualify for a loan, a person must make at least $30,000
-* and have been working at their current job for at least 2 years
+* and that person have been working at their current job for at least 2 years
 */
 
 import java.util.Scanner;
