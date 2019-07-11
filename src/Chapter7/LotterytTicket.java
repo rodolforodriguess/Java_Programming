da8e09a6ccd2953bcff0dbec665f0b19bf12b733
Rodolfo Rodrigues <rodolfo@sk8.com.br>
2019-07-11 19:51:37 -0300

    Arrays lesson two

--- src/Chapter7/LotterytTicket.java
+++ src/Chapter7/LotterytTicket.java
@@ -1,5 +1,6 @@
 package Chapter7;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class LotterytTicket {
@@ -9,6 +10,7 @@ public class LotterytTicket {
 
     public static void main(String[] args){
         int[] ticket = generateNumbers();
+        Arrays.sort(ticket);
         printTicket(ticket);
     }
 
@@ -19,10 +21,55 @@ public class LotterytTicket {
         Random random = new Random();
 
         for(int i=0; i<LENGTH; i++){
-            ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            int randomNumber;
+
+            /*
+            Generate random number then search to make it doesn't
+            already exist in the array. If it foes, regenerate the number, and search again.
+             */
+            do{
+                randomNumber = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            }while(search(ticket, randomNumber));
+            //Number is unique if we get here. Add it to the array
+            ticket[i] = randomNumber;
         }
         return ticket;
     }
+
+    /**
+     * Does a sequential search on the array to find a value
+     * @param array Array to search through
+     * @param numberToSearchFor Value to search for
+     * @return true if found and false if not
+     */
+    public static boolean search(int[] array, int numberToSearchFor){
+            /*
+            This is called an enhanced loop.
+            It iterates through 'array' and each time assigns the current element to 'value'
+             */
+        for (int value : array){
+            if (value == numberToSearchFor){
+                return true;
+            }
+        }
+        /*
+        If we've reached this point, then the entire array was searched and
+        the value was not found
+         */
+         return false;
+    }
+
+    public static boolean binarySearch(int[] array, int numberToSearchFor){
+        Arrays.sort(array);
+        int index = Arrays.binarySearch(array, numberToSearchFor);
+        if (index >= 0){
+            return true;
+        }
+        else return false;
+    }
+
+
+
 
     public static void printTicket(int ticket[]){
         for(int i=0; i<LENGTH; i++){
