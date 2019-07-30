fbf36a70635329e3bf368df7cf2c2975537dacdc
Rodolfo Rodrigues <rodolfo@sk8.com.br>
2019-07-29 23:11:51 -0300

    Practsing Inheritance

---
+++ src/chapter9/TasteTester.java
@@ -0,0 +1,18 @@
+package chapter9;
+
+public class TasteTester {
+
+    public static void main(String[] args){
+        BirthdayCake birth = new BirthdayCake();
+        birth.setPrice(50);
+
+        System.out.println("The BirthdayCake is " + birth.getFlavor() + " and the price is " + birth.getPrice());
+        
+        WeddingCake wed = new WeddingCake();
+        wed.setPrice(45);
+
+        System.out.println("The WeddingCake is " + wed.getFlavor() + " and the price is " + wed.getPrice());
+    }
+
+
+}
